08e7eff9ad894c14c399b30af7f761e4155d6aad
Azor <mateusazorfrutuoso@gmail.com>
2022-04-13 00:39:10 -0300

    Register Product & List Products
    - Register: Done, but has bugs on stock value, and pointers alocation.
    - List: Done.

--- AmazonCC.c
+++ AmazonCC.c
@@ -30,11 +30,48 @@ typedef struct _cart Cart;
 // Definitions of functions
 
 int verifyMenuInput(int input){ // Verify if the user input is valid to navigate in main menu
+
     while(input > 5 || input < 0){ // Loop while the input is invalid
         printf("Input not valid, try again: ");
         scanf("%d", &input);
     }
+
     return input; // When valid return input
+}
+
+int verifyId(int id, ProductKeys *listReference){ 
+
+    int alreadyExist, loop = 1; // Declaration of extra variables
+
+    if(listReference->head->next == NULL){ // If there is no elements in the list
+        return id; // Stop the function now
+    }
+
+    Product *helper; // Create an extra pointer
+    helper = malloc(sizeof(Product));
+    helper = listReference->head->next; // Set it to point to the first element of the list
+
+    while(loop == 1){ // Main loop
+    
+        while(helper->next != NULL){ // Loop to verify all products of the list
+            if(helper->id == id){
+                alreadyExist = 1;
+                break;
+            }
+        }
+
+        if(alreadyExist == 1){ // If id already exists
+            printf("ID already exist, please type another: ");
+            scanf("%d", &id); // Get another
+        }else{
+            loop = 0; // Break the main loop
+        }
+    }
+
+    helper = NULL; // Free the alocated memory of the extra pointer
+    free(helper);
+    
+    return id;
 }
 
 // Main program
@@ -42,6 +79,15 @@ int verifyMenuInput(int input){ // Verify if the user input is valid to navigate
 int main(){
 
     int switcher = 1; // Variable to switch between menus
+
+    ProductKeys *listReference; // Create the struct that stores the head and tail of the products list
+    listReference = malloc(sizeof(ProductKeys));
+    listReference->head = malloc(sizeof(Product));
+    listReference->head->next = NULL; // Configure head
+    listReference->head->prev = NULL;
+    listReference->tail = malloc(sizeof(Product));
+    listReference->tail->next = NULL; // Configure tail
+    listReference->tail->prev = NULL;
 
     printf("\n                                           _____ _____ \n     /\\                                   / ____/ ____|\n    /  \\   _ __ ___   __ _ _______  _ __ | |   | |     \n   / /\\ \\ | '_ ` _ \\ / _` |_  / _ \\| '_ \\| |   | |     \n  / ____ \\| | | | | | (_| |/ / (_) | | | | |___| |____ \n /_/    \\_\\_| |_| |_|\\__,_/___\\___/|_| |_|\\_____\\_____|\n\n"); // Print the logo
 
@@ -54,30 +100,64 @@ int main(){
 
         switch(switcher){ // Does something according to what the user typed
 
-            case(0): // Exit System
+            case(0): ; // Exit System
+
                 break; // Exit the main loop making the code end
             
-            case(1): // Register Product
+            case(1): ; // Register Product
+
+                Product *placeholder; // Create the product
+                placeholder = malloc(sizeof(Product));
+                printf("\nType the ID of the Product: "); // Scan and storage values on new product
+                scanf("%d", placeholder->id);
+                placeholder->id = verifyId(placeholder->id, listReference); // Verify if the id is valid
+                printf("\nType the NAME of the Product: ");
+                scanf("%s", placeholder->name);
+                printf("\nType the PRICE of the Product: ");
+                scanf("%f", &placeholder->price);
+                printf("\nType the STOCK of the Product: ");
+                scanf("%d", placeholder->stock);
+                placeholder->next = NULL; // Set the pointers to NULL
+                placeholder->prev = NULL;
+
+                Product *helper; // Create an extra pointer
+                helper = malloc(sizeof(Product));
+                helper = listReference->tail->prev; // Set it to point to the last element of the list
+
+                helper->next = placeholder; // Make the tail and the last element point to the new
+                listReference->tail->prev = placeholder; // (if the list is empty, the "last element" will be the head)
+                placeholder->prev = helper; // And the new element point to both
+                placeholder->next = listReference->tail;
+
+                helper = NULL; // Free the alocated memory of the extra pointer
+                free(helper);
+
+            case(2): ; // List Products
+                
+                Product *helper; // Create an extra pointer
+                helper = malloc(sizeof(Product));
+                helper = listReference->head->next; // Set it to point to the first element of the list
+
+                while(helper->next != NULL){
+                    printf("\nId: %d\nName: %s\nPrice: %f\nStock: %d", helper->id, helper->name, helper->price, helper->stock);
+                }
+
+                helper = NULL; // Free the alocated memory of the extra pointer
+                free(helper);
+
+            case(3): ; // Search Product
                 printf("WIP");
                 break;
 
-            case(2): // List Products
+            case(4): ; // Delete Product
                 printf("WIP");
                 break;
 
-            case(3): // Search Product
+            case(5): ; // Buy Products
                 printf("WIP");
                 break;
 
-            case(4): // Delete Product
-                printf("WIP");
-                break;
-
-            case(5): // Buy Products
-                printf("WIP");
-                break;
-
-            default: // If somehow the user bypass the input verification and type a invalid number, end the program
+            default: ; // If somehow the user bypass the input verification and type a invalid number, end the program
                 printf("ERROR - Invalid Main Menu Input");
                 break;
         }
